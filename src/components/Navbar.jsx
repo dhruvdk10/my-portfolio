7d0646cba4239f613dadefbb86df007b134c92df
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2026-03-17 23:05:52 +0530

    'lu'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,34 @@ import { MdDownload } from "react-icons/md";
 
 function Navbar() {
 
+  const navbarData = {
+    name: "DHRUV KAPOOR",
+    resume: {
+      text: "RESUME",
+      file: "/Resume-Dhruv-Kapoor.pdf"
+    }
+  };
+
   return (
-
     <nav className="custom-navbar fixed-top">
-
       <div className="container nav-top">
 
         <a className="brand-logo" href="#">
-          DHRUV KAPOOR
+          {navbarData.name}
         </a>
 
         <a
-          href="./Resume-Dhruv-Kapoor.pdf"
+          href={navbarData.resume.file}
           download
           className="resume-btn"
         >
           <MdDownload />
-          <span>RESUME</span>
+          <span>{navbarData.resume.text}</span>
         </a>
 
       </div>
-
     </nav>
-
-  )
-
+  );
 }
 
 export default Navbar;
