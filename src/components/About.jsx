2b92e5a897f79ac7c4d747d8bcc48c61a76914e2
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2026-03-18 21:51:15 +0530

    'lu'

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,14 +31,14 @@ function About() {
           {/* Left side text */}
           <div className="col-lg-7 col-md-12 mb-4 mb-lg-0">
             <div className="terminal-text">
-              <p className="line line1">&gt; <span>Hey</span> <span>there</span></p>
-              <p className="line line2">&gt; <span>I'm</span> <span>Dhruv</span> <span>Kapoor,</span> <span>a</span> <span>curious</span> <span>web</span> <span>developer</span></p>
-              <p className="line line3">&gt; <span>I</span> <span>love</span> <span>turning</span> <span>ideas</span> <span>into</span> <span>interactive</span> <span>web</span> <span>projects</span></p>
-              <p className="line line4">&gt; <span>My</span> <span>tools:</span> <span>HTML,</span> <span>CSS,</span> <span>JavaScript,</span> <span>Bootstrap</span> <span>and</span> <span>React</span></p>
-              <p className="line line5">&gt; <span>Currently</span> <span>exploring</span> <span>the</span> <span>MERN</span> <span>stack</span> <span>world</span></p>
-              <p className="line line6">&gt; <span>Outside</span> <span>coding</span> <span>I</span> <span>play</span> <span>football</span> <span>and</span> <span>chess</span></p>
-              <p className="line line7">&gt; <span>Always</span> <span>learning</span> <span>new</span> <span>things</span> <span>about</span> <span>web</span> <span>development</span></p>
-              <p className="line line8">&gt; <span>Let's</span> <span>create</span> <span>something</span> <span>awesome</span> <span>together</span></p>
+              <p className="line line1">&gt; <span>Hey</span> <span>again</span></p>
+              <p className="line line2">&gt; <span>I'm</span> <span>Dhruv</span> <span>Kapoor</span></p>
+              <p className="line line3">&gt; <span>A</span> <span>curious</span> <span>web</span> <span>developer</span></p>
+              <p className="line line4">&gt; <span>I</span> <span>build</span> <span>clean</span> <span>and</span> <span>interactive</span> <span>projects</span></p>
+              <p className="line line5">&gt; <span>I</span> <span>enjoy</span> <span>creating</span> <span>smooth</span> <span>user</span> <span>experiences</span></p>
+              <p className="line line6">&gt; <span>Currently</span> <span>exploring</span> <span>the</span> <span>MERN</span> <span>stack</span></p>
+              <p className="line line7">&gt; <span>Outside</span> <span>coding</span> <span>I</span> <span>play</span> <span>football</span> <span>and</span> <span>chess</span></p>
+              <p className="line line8">&gt; <span>Let’s</span> <span>create</span> <span>something</span> <span>awesome</span> <span>together</span></p>
             </div>
           </div>
 
